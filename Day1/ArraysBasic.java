dbbe4c947625362433eb4e97f847e2f7c9b67cd2
Shreenu <shreenu.vasu.lakkoji@gmail.com>
2025-07-06 16:01:58 +0530

    ✅ Day1 structure: arrays, loops, notes added

---
+++ Day1/ArraysBasic.java
@@ -0,0 +1,6 @@
+// Day 1 - Arrays Basic Problems
+public class ArraysBasics {
+    public static void main(String[] args) {
+        // You can add problems like max, second max, reverse, sorted check
+    }
+}
---
+++ Day1/JavaLoopsBasics.java
@@ -0,0 +1,6 @@
+// Day 1 - Java Basics (Loops, Variables, Input)
+public class JavaLoopsBasics {
+    public static void main(String[] args) {
+        // Tasks: Fibonacci, Prime Check, Sum of Digits
+    }
+}
